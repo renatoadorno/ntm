c0143cff5c9dea74539092d2d0957baa8ac809ba
renato <renatowillian.2000@gmail.com>
2022-05-16 01:52:10 -0300

    :construction: feat: criando aliases

--- install.sh
+++ install.sh
@@ -1,19 +1,47 @@
 #!/bin/zsh
 
-# DIRETORIO=$HOME/.ntm
-ARQUIVO=$HOME/.zs
+VERIFICA E INSTALA
+ZIP='ntm.zip'
+URL=
+wget -O $ZIP $URL
+unzip $ZIP -d $HOME/
 
-# mkdir $HOME/.ntm
+if [ -f $ZIP ]
+then
+  rm $ZIP
+fi
 
-# touch $ARQUIVO 
+BASHRC=$HOME/.bashrc
+exec_bash() {
+cat >> $BASHRC << EOF
 
-cat >> $ARQUIVO << EOF
+# ntm
+if [ -f ~/.ntm/ntm_aliases  ]; then
+  . ~/.ntm/ntm_aliases 
+fi
 
-  # ntm
-  if [ -f ~/.zshrc_aliases ]; then
-    . ~/.zshrc_aliases
-  fi
 EOF
+}
+
+ZSHRC=$HOME/.zshrc
+exec_zsh() {
+cat >> $ZSHRC << EOF
+
+# ntm
+if [ -f ~/.ntm/ntm_aliases  ]; then
+  . ~/.ntm/ntm_aliases 
+fi
+
+EOF
+}
+
+if [ -f ~/.zshrc ]; then
+  exec_zsh
+fi
+
+if [ -f ~/.bashrc ]; then
+  exec_bash
+fi
 
 # Teste o código de retorno para constatar se o arquivo foi criado ou não
 if [ $? -eq 0 ]; then
